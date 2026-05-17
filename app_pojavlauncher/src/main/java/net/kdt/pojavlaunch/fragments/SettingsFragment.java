e8691222361b236e6b82cd88ee0e89edee3bd4c2
shubhamvalacha14-ctrl <shubhamvalacha14@gmail.com>
2026-05-17 14:37:46 +0530

    Update SettingsFragment.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/SettingsFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/SettingsFragment.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import com.launch.nova.debug.R;
+import net.kdt.pojavlaunch.R;
 
 public class SettingsFragment extends Fragment {
 
